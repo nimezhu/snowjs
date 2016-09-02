0a750462ec094bf0ca5ab050a55f8eedc82e497e
zhuxp <nimezhu@gmail.com>
2016-09-02 11:17:57 -0400

    init

---
+++ web.go
@@ -0,0 +1,63 @@
+package snow
+//go:generate go-bindata-assetfs -pkg snow static/...
+
+const (
+	VERSION = "0.0.1"
+)
+/* VERSION 0.0.1 Host Libraries:
+ * lib/
+ *     angular.min.js  
+ *	   bootstrap.min.js    
+ *     d3.v3.min.js   
+ *     list.min.js
+ *	   app.min.js      
+ *     d3-queue.v2.min.js  
+ *     jquery.min.js
+ * css/ 
+ *    AdminLTE.min.css  
+ *    bootstrap.min.css  
+ *    skin-black.min.css
+ * fonts/
+ *  glyphicons-halflings-regular.eot  
+ *	glyphicons-halflings-regular.woff
+ *	glyphicons-halflings-regular.svg  
+ *	glyphicons-halflings-regular.woff2
+ *	glyphicons-halflings-regular.ttf
+ */
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func CssHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/css")
+	ps := mux.Vars(r)
+	bytes, err := Asset("static/css/" + ps["file"])
+	if err != nil {
+		log.Println(err)
+	}
+	io.WriteString(w, string(bytes))
+}
+func JsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/JavaScript")
+	ps := mux.Vars(r)
+	bytes, err := Asset("static/lib/" + ps["file"])
+	if err != nil {
+		log.Println(err)
+	}
+	io.WriteString(w, string(bytes))
+}
+
+func FontHandler(w http.ResponseWriter, r *http.Request) {
+	ps := mux.Vars(r)
+	bytes, err := Asset("static/fonts/" + ps["file"])
+	if err != nil {
+		log.Println(err)
+	}
+	io.WriteString(w, string(bytes))
+}
+
